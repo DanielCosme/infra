8975e3b62f25124ace35bc4d3ce73574b4ef81b9
Daniel Cosme <cosmedaniel8@gmail.com>
2026-01-20 21:52:59 -0500

    Rewrite setup script in ruby

--- vps/setup_base.sh
+++ vps/setup_base.sh
@@ -26,8 +26,8 @@ while read IP FQDN HOST; do
     ssh -n root@${IP} "echo $VM_USER:$VM_PASSWORD | chpasswd"
     ssh -n root@${IP} "mkdir /home/$VM_USER/.ssh"
     ssh -n root@${IP} "cp ~/.ssh/authorized_keys /home/$VM_USER/.ssh/authorized_keys && chown -R $VM_USER:$VM_USER /home/$VM_USER/.ssh"
-    ssh -n root@${IP} "curl -s https://install.crowdsec.net | sudo sh && apt install -y crowdsec crowdsec-firewall-bouncer-iptables"
-    ssh -n root@${IP} "systemctl reload crowdsec"
+    # ssh -n root@${IP} "curl -s https://install.crowdsec.net | sudo sh && apt install -y crowdsec crowdsec-firewall-bouncer-iptables"
+    # ssh -n root@${IP} "systemctl reload crowdsec"
     scp ./config/sshd/sshd_config root@${IP}:/etc/ssh/sshd_config
     ssh -n root@${IP} "ufw allow OpenSSH && ufw --force enable"
     ssh -n root@${IP} "systemctl reload ssh && systemctl reboot"
